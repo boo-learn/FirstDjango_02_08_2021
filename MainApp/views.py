175487f68bd95055847928edf4d7564da096b070
boo <boo>
2021-08-04 10:47:23 -0700

    task 3 complete

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,23 +1,11 @@
 from django.http import Http404
 from django.shortcuts import render, HttpResponse
-
-ITEMS = [
-    {"id": 1, "name": "Кроссовки abibas", "quantity": 5},
-    {"id": 2, "name": "Куртка кожаная", "quantity": 2},
-    {"id": 3, "name": "Coca-cola 1 литр", "quantity": 12},
-    {"id": 4, "name": "Картофель фри", "quantity": 0},
-    {"id": 5, "name": "Кепка", "quantity": 124},
-]
+from MainApp.models import Item
+from django.core.exceptions import ObjectDoesNotExist
 
 
-# Create your views here.
 def index(request):
-    context = {
-        "name": "Евгений",
-        "surname": "Юрченко",
-        "hobbies": ["programming", "bike", "sleep"]
-    }
-    return render(request, "index.html", context)
+    return render(request, "index.html")
 
 
 def about(request):
@@ -32,15 +20,17 @@ def about(request):
 
 
 def items(request):
-    context = {"items": ITEMS}
+    items = Item.objects.all()
+    context = {"items": items}
     return render(request, "items_list.html", context)
 
 
 def item_details(request, id):
-    for item in ITEMS:
-        if item['id'] == id:
-            context = {
-                "item": item
-            }
-            return render(request, "item_page.html", context)
-    raise Http404
+    try:
+        item = Item.objects.get(pk=id)
+    except ObjectDoesNotExist:
+        raise Http404
+    context = {
+        "item": item
+    }
+    return render(request, "item_page.html", context)
