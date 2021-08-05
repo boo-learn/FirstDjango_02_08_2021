085e3e9c62c3c6ca3378b10a53c075f53dfee4ed
boo <boo>
2021-08-05 09:55:00 -0700

    add task-3-2

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from MainApp import views
 
@@ -8,4 +10,4 @@ urlpatterns = [
     path('item/', views.items),
     path('items/', views.items),
     path('item/<int:id>', views.item_details),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
